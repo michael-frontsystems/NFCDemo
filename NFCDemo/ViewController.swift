f93cfe611aa0535fed4e36b1d30e6b49d1f54cd2
Michael Redoble <michael@frontsystems.no>
2018-04-06 10:56:29 +0200

    Initial Commit

---
+++ NFCDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFCDemo
+//
+//  Created by Michael Redoble on 06/04/2018.
+//  Copyright © 2018 Michael Redoble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
